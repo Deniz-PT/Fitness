c9ae93cfe287fb9945e831c12e4296515cf59f9b
Deniz Ugur <deniz.ugur2006@hotmail.com>
2022-10-13 09:21:01 +0200

    RWD Navbar

--- Fanugu.js
+++ Fanugu.js
@@ -2,3 +2,12 @@ function showPopup(Kaufen) {
   const popup = document.getElementById(Kaufen);
   popup.classList.toggle("show");
 }
+
+function RWDnavbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
